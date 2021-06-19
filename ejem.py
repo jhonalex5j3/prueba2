7eef39cb7c8164e81d8c17e834db173e302f4383
jhonalex5j3 <informaticatecnolog10@gmail.com>
2021-06-19 16:25:28 -0500

    ahora si se ven los commit

--- ejem.py
+++ ejem.py
@@ -4,3 +4,4 @@ print("hola tu nombre completo es: {} {}  ".format(nombre,apellido))
 print(f"hola tu nombre es {nombre} {apellido}")
 str saludo="hola mundo, nuevo camkkbio"
 print(saludo)
+print("excelente")
