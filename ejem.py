52a0aac43ced19f4eb68b747849f31103bf3affc
jhonalex5j3 <informaticatecnolog10@gmail.com>
2021-06-19 16:07:17 -0500

    agregados ejem.py, hola 2, hola y saludos

---
+++ ejem.py
@@ -0,0 +1,6 @@
+nombre=("jhon")
+apellido=("jerez")
+print("hola tu nombre completo es: {} {}  ".format(nombre,apellido))
+print(f"hola tu nombre es {nombre} {apellido}")
+str saludo="hola mundo, nuevo camkkbio"
+print(saludo)
